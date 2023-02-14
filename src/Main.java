1c90ecef30bedc05e98734f8f8dff072c113f5a1
meder <meder.tynychbekov@gmail.com>
2023-02-14 18:52:51 +0300

    Добавление дополнительных методов.

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,5 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        String text = "Hello my name is Meder";
-
-        System.out.println(text);
-
-        String[] textMassive = text.split(" ");
-
-        System.out.println(Arrays.toString(Arrays.stream(textMassive).toArray()));
     }
 }
